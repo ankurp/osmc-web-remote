b752d15987b0dcc3bcbbb263212051a0b89cb9e6
Ankur Patel <ankur.patel@ymail.com>
2015-05-14 22:42:45 -0400

    Setting up Remote UI

--- app/entry.js
+++ app/entry.js
@@ -3,4 +3,8 @@ require('!style!css!sass!./scss/main.scss');
 require('./js/xbmc-init');
 
 var $ = require('jquery');
-$('body').append(require('jade!./template/index.jade')());
+var html = require('jade!./template/index.jade')();
+var bgImgURL = require("file!./img/splash.png");
+
+$('body').append(html);
+$('body').css({'background-image': 'url(' + bgImgURL + ')'});
--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 // returns a Compiler instance
 var compiler = webpack({
-  context: __dirname + '/app',
-  entry: './entry',
-  output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({title: 'OSMC'})
-  ]
+    context: __dirname + '/app',
+    entry: './entry',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'OSMC'
+        })
+    ]
 });
-
-compiler.run(function(err, stats) {
-
-});
+compiler.run(function(err, stats) {});
 // or
 compiler.watch({ // watch options:
     aggregateTimeout: 300, // wait so long for more changes
     poll: true // use polling instead of native watchers
-    // pass a number to set the polling interval
-}, function(err, stats) {
-
-});
+        // pass a number to set the polling interval
+}, function(err, stats) {});
